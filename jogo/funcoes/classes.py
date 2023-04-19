311e038fdf3ea6fec5e85204c2cabd3f2f7ea48d
Henrique Mayor <henriquessm@al.insper.edu.br>
2023-04-19 13:47:38 -0300

    inimigo se move, jogador pula e duas telas

--- jogo/funcoes/classes.py
+++ jogo/funcoes/classes.py
@@ -1,9 +1,12 @@
 import pygame
 
 
+#TODO: Não precisa do dicionário assets (so precisa acertar um jeito de importar a tela), arrumar funcoes_do_jogo.py
+
+
 class TelaInicial():
     def __init__(self, assets):
-        self.dic = assets
+        self.dicionario = assets
         self.tela = assets['tela']
         self.fonte = pygame.font.Font(assets['fonte'], 50)
         self.texto = self.fonte.render('Tela inicial', True, (0, 0, 0))
@@ -20,39 +23,45 @@ class TelaInicial():
                 pygame.quit()
                 return False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
+                if event.key == pygame.K_TAB:
                     self.tem_que_trocar = True
         return True
     
     def troca_tela(self):
         if self.tem_que_trocar:
-            return TelaJogo(self.dic)
+            return TelaJogo(self.dicionario)
         else:
             return self
 
 class TelaJogo():
     def __init__(self, assets):
+
         self.fonte = pygame.font.Font(assets['fonte'], 50)
-        self.texto = self.fonte.render('Tela Jogo', True, (0, 0, 0))
-        self.dic = assets
+        self.texto = self.fonte.render('Tela Jogo', True, (0, 255, 0))
+        self.dicionario = assets
+        #Adicionei essas imagens so para testar e dps mudar
+        self.fundo = assets['fundo']
+        self.chao = assets['ground']
         self.tela = assets['tela']
+
         self.tem_que_trocar = False
-        self.x = 800
-        self.y = 600
+
+
+        self.inimigo = Inimigo()
+        self.jogador = Jogador()
 
     def desenha(self):
-        self.tela.fill((255, 255, 0))
+        self.tela.blit(self.fundo, (0, 0))
         self.tela.blit(self.texto, (250, 0))
-        pygame.draw.circle(self.tela, (0, 0, 0), (self.x, self.y), 50)
+        self.tela.blit(self.chao, (0, 620))
+
+        self.inimigo.update()
+        self.tela.blit(self.inimigo.image, self.inimigo.rect)
+
+        self.jogador.update()
+        self.tela.blit(self.jogador.image, self.jogador.rect)
+
         pygame.display.update()
-
-
-    def troca_tela(self):
-        if self.tem_que_trocar:
-            return TelaInicial(self.dic)
-        else:
-            return self
-
 
     def update(self):
         for event in pygame.event.get():
@@ -60,9 +69,77 @@ class TelaJogo():
                 pygame.quit()
                 return False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
+                if event.key == pygame.K_TAB:
+                    #a condicao de trocar tela vira true
                     self.tem_que_trocar = True
-        self.x -= 1
-        self.y -= 1
+            self.jogador.pulo_jogador(event)
+        self.jogador.gravidade += 0.1
         return True
     
+    def troca_tela(self):
+        if self.tem_que_trocar:
+            return TelaInicial(self.dicionario)
+        else:
+            return self
+
+class Jogador(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        #Adicionei essas imagens so para testar e dps mudar
+        #Cria retangulo e aumenta a imagem
+        self.image = pygame.image.load('jogo/assets/jogador_provisorio.png').convert_alpha()
+        self.image = pygame.transform.scale_by(self.image, 4)
+        self.rect = self.image.get_rect()
+
+        #Coordenadas
+        self.rect.centerx = 30
+        self.rect.centery = 560
+
+        self.x = 0
+        self.delta_t = 0
+        #Velocidade y
+
+        self.vely = -21
+
+        #Gravidade
+        self.gravidade = 0
+
+
+    def update(self):
+        
+        #Faz o jogador cair
+        self.rect.centery += self.gravidade
+
+
+        #Não deixa o jogador passar do chão
+        if self.rect.centery >= 560:
+            self.rect.centery = 560
+            
+
+    def pulo_jogador (self, event):
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+
+                #Faz o jogador pular
+                if self.rect.bottom >= 560:
+                    self.gravidade = -8
+
+class Inimigo (pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load('jogo/assets/inimigo_provisorio.png').convert_alpha()
+        self.image = pygame.transform.scale_by(self.image, 4)
+        self.image = pygame.transform.flip(self.image, True, False)
+
+        self.rect = self.image.get_rect()
+
+        self.rect.centerx = 1280
+        self.rect.centery = 600
+    
+    def update(self):
+        self.rect.centerx -= 1
+
+        if self.rect.centerx <= -100:
+            self.rect.centerx = 1280
--- jogo/funcoes/funcoes_do_jogo.py
+++ jogo/funcoes/funcoes_do_jogo.py
@@ -1,16 +1,20 @@
 import pygame
 from classes import TelaInicial, TelaJogo
 
-
 def inicializa():
     pygame.init()
-    tela = pygame.display.set_mode((800,600))
+
+
+    tela = pygame.display.set_mode((1280,720))
     fonte = pygame.font.get_default_font()
     pygame.display.set_caption('Knight Runner')
 
     assets ={
         'tela': tela,
         'fonte': fonte,
+        #Adicionei essas imagens so para testar e dps mudar
+        'fundo': pygame.transform.scale((pygame.image.load('jogo/assets/fundo_provisorio.png').convert_alpha()), (1280, 720)),
+        'ground': pygame.transform.scale((pygame.image.load('jogo/assets/ground_provisorio.png').convert_alpha()), (1280, 300)),
 
     }
     return assets
@@ -25,9 +29,10 @@ def recebe_eventos():
 def game_loop():
     assets = inicializa()
     tela_atual = TelaInicial(assets)
-    while tela_atual.update():
+    while tela_atual.update(): 
         tela_atual = tela_atual.troca_tela()
         tela_atual.desenha()
+
 
 if __name__ == '__main__':
     game_loop()
